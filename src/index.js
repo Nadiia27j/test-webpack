b7f86d4ebd11d2a085e33a75f3db500ce23ad9f1
Надія Ткач <nadiya77723@gmail.com>
2022-09-03 11:08:01 +0300

    ''

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
-import name from './module-1';
-import './css/common.scss';
-import './css/button.scss';
 
-console.log(name);
-console.log('Це index.js');
+// console.log(name);
+// console.log('Це index.js');
 
-// Створює новий запис у сховищі у вигляді рядка
-localStorage.setItem('my-data', JSON.stringify({name: 'Mango', age: 2}));
+// // Створює новий запис у сховищі у вигляді рядка
+// localStorage.setItem('my-data', JSON.stringify({name: 'Mango', age: 2}));
 
-// повертає значення зі сховища
-const saveData = localStorage.getItem('my-data');
-const parsedData = JSON.parse(saveData);
-console.log(parsedData); 
+// // повертає значення зі сховища
+// const saveData = localStorage.getItem('my-data');
+// const parsedData = JSON.parse(saveData);
+// console.log(parsedData); 
 
 
-// Шаблонізація_____________________
+// // Шаблонізація_____________________
 
 
-'<div>{{ name }}<div>'
+// '<div>{{ name }}<div>'
 
-const template = SuperTEmplatingEngine.compile('<div>{{ name }}<div>');
+// const template = SuperTEmplatingEngine.compile('<div>{{ name }}<div>');
 
-template({name: 'Mango'});  // '<div>Mango<div>'
+// template({name: 'Mango'});  // '<div>Mango<div>'
+
+
---
+++ src/scripts/refs.js
@@ -0,0 +1,6 @@
+
+export const refs = {
+    form: document.querySelector('#form'),
+    input: document.querySelector("input"),
+    list: document.querySelector('.list'),
+}
