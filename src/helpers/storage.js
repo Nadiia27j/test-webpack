78c41f2ad048776ce6c43268ec74f4f219cb1088
Надія Ткач <nadiya77723@gmail.com>
2022-09-06 16:45:16 +0300

    dd

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -1,14 +1,14 @@
-const storage = {
-    addItem() {},
-    reedItem(key, defaultValue) {
-        try {
-           const result = JSON.parse(localStorage.getItem(key));
-           return result ? result : defaultValue;
-        }catch (error) {
-            return null; 
-        }
-    },
-    deleteItem() {},
-    clear() {},
+// const storage = {
+//     addItem() {},
+//     reedItem(key, defaultValue) {
+//         try {
+//            const result = JSON.parse(localStorage.getItem(key));
+//            return result ? result : defaultValue;
+//         }catch (error) {
+//             return null; 
+//         }
+//     },
+//     deleteItem() {},
+//     clear() {},
 
-}
+// }
--- src/index.js
+++ src/index.js
@@ -7,64 +7,71 @@
 // const saveData = localStorage.getItem('my-data');
 // const parsedData = JSON.parse(saveData);
 // console.log(parsedData); 
-import { refs } from "./scripts/refs";
+// import { refs } from "./scripts/refs";
 
 
 
-// функція створення лішки
-const createLi = (text) => `<li data-id='${text}'>${text}</li>`;
+// // функція створення лішки
+// const createLi = (text) => `<li data-id='${text}'>${text}</li>`;
     
-// вставка розмітки на екран
-  const addLiToList = (markdown) => {
-    refs.list.insertAdjacentHTML('beforeend', markdown);
-}
+// // вставка розмітки на екран
+//   const addLiToList = (markdown) => {
+//     refs.list.insertAdjacentHTML('beforeend', markdown);
+// }
   
-const handlerSubmit = (e) => {
-    // забороняєм перезавантажувати сторінку
-  e.preventDefault();            
-  //   дістаємо значення інпуту
-  const value = refs.input.value; 
-   //   value запишеться в text  і передасться в лішку 
-  const liItem = createLi(value);  
-   //   лішка вставиться в ул  
-  addLiToList(liItem);             
-  //   отримали доступ до сторедж
-  const list = localStorage.getItem('list');
-  try { // якщо клас ліст є то парсимо ліст якщо ні то створєм новий []
-    const listData = list ? JSON.parse(list) : [];
-  //   пушимо значення в ул
-    listData.push(value);
-    // перетворюємо в JSON
-    const updatedList = JSON.stringify(listData);
-    // записуємо новий json в сторедж
-    localStorage.setItem('list', updatedList);  
-  }catch (error) {
-    console.log('parsing error');
-  }
-  refs.input.value.reset;  
+// const handlerSubmit = (e) => {
+//     // забороняєм перезавантажувати сторінку
+//   e.preventDefault();            
+//   //   дістаємо значення інпуту
+//   const value = refs.input.value; 
+//    //   value запишеться в text  і передасться в лішку 
+//   const liItem = createLi(value);  
+//    //   лішка вставиться в ул  
+//   addLiToList(liItem);             
+//   //   отримали доступ до сторедж
+//   const list = localStorage.getItem('list');
+//   try { // якщо клас ліст є то парсимо ліст якщо ні то створєм новий []
+//     const listData = list ? JSON.parse(list) : [];
+//   //   пушимо значення в ул
+//     listData.push(value);
+//     // перетворюємо в JSON
+//     const updatedList = JSON.stringify(listData);
+//     // записуємо новий json в сторедж
+//     localStorage.setItem('list', updatedList);  
+//   }catch (error) {
+//     console.log('parsing error');
+//   }
+//   refs.input.value.reset;  
+// }
+
+// refs.form.addEventListener('submit', handlerSubmit);
+
+// // згенерувати дані з локал сторедж на екран при завантаженні сторінки
+// const handleMount = () => {
+//     const list = localStorage.getItem('list');
+//  try {
+//     const savedList = JSON.parse(list);
+//     const markdown = savedList.reduce((acc, text) => acc +  createLi(text), ""); 
+//     addLiToList(markdown);
+//  } catch (error) {
+//     console.log('parsing error');
+//  }
+// }
+// // запусти фун ю для завантаження сторінки
+// addEventListener('DOMContentLoaded', handleMount);
+
+
+const logMessage = () => {
+  console.log('log через 3 сек');
 }
 
-refs.form.addEventListener('submit', handlerSubmit);
+console.log('до виклику setTimeout');
 
-// згенерувати дані з локал сторедж на екран при завантаженні сторінки
-const handleMount = () => {
-    const list = localStorage.getItem('list');
- try {
-    const savedList = JSON.parse(list);
-    const markdown = savedList.reduce((acc, text) => acc +  createLi(text), ""); 
-    addLiToList(markdown);
- } catch (error) {
-    console.log('parsing error');
- }
-}
-// запусти фун ю для завантаження сторінки
-addEventListener('DOMContentLoaded', handleMount);
+setTimeout(() => {
+  console.log('колбек для сеттаймаут');
+}, 2000)
 
-
-
-
-
-
+console.log('після виклику setTimeout');
 
 
 
